5711b4523ddcd60354e9f0d4ca0a8fe4c816d2e4
OfficialVic <onaopeadeoye594@gmail.com>
2023-08-24 16:22:52 +0100

    functionalities and UI addition

--- main.js
+++ main.js
@@ -1,73 +1,84 @@
-// Collect name and code and show welcome message
-const name = prompt(`Please Enter your first name`)
-let code;
-do {
-    code = prompt(`Enter Polaris USSD Code`)
-}while(code !== "*833#")
-
-if ( code == "*833#") {
-        alert(`Welcome ${name}! Click 'Ok' to continue.`);
-        // Display options for navigation
+function polarisUSSD () {
+    // Collect name and code and show welcome message
+    const name = prompt(`Please Enter your first name`)
+    let code;
+    do {
+        code = prompt(`Enter Polaris USSD Code`)
+    }while(code !== "*833#")
+    
+    alert(`Welcome ${name}! Click 'Ok' to continue.`);
+    // Display options for navigation
+    const balance = 450000;
+    let amount;
+    let pin;
+    let newBalance;
+    let currency = '\u20A6';
+    let USSDTransaction = () => {
         let option = +prompt(`Kindly select your preferred option \n
         1. Check account balance
         2. Airtime Purchase
-        3. Transfer`)
-    
-        const balance = 45000;
-        let amount;
-        let pin;
-        let newBalance; 
+        3. Transfer`)    
         if (option == 1) {
             let account = prompt(`Enter your account number`)
-            alert(`Your account balance is ${balance}`);
+            alert(`Your account balance is ${currency}${balance}`);
+            another()
         } else if(option == 2 ) {
-            let toWho = +prompt(`Select Airtime Top Up choice
-            1. Self
-            2. Others`)
-            switch(toWho) {
-                case 1:
-                    amount = +prompt(`Enter the amount to top up`);
-                    pin = +prompt(`Enter your pin`)
-                    newBalance = balance - amount;
-                    switch(pin){
-                        case 1234:
-                            alert(`Recharge Sucessful!!! Your new account balance =  ${currency}${newBalance}`);
-                        break;
-                        default:
-                            alert(`Enter the correct pin`);
-                    }
-                break;
-                case 2:
-                    let otherNumber = prompt(`Enter the beneficiary number`);
-                    amount = +prompt(`Enter the amount to top up`);
-                    pin = +prompt(`Enter your pin`)
-                    newBalance = balance - amount;
-                    // amount = +prompt(`Enter the amount to top up`);
-                    // pin = +prompt(`Enter your pin`)
-                    // newbalance = balance - amount;
-                    switch(pin){
-                        case 1234:
-                            alert(`Recharge Sucessful!!! Your new account balance = ${newBalance}`);
-                        break;
-                        default:
-                            alert(`Enter the correct pin`);
-                    }
-                break;
-                default:
-                    alert(`Enter the available option`);
+            let airtime = () => {
+                let toWho = +prompt(`Select Airtime Top Up choice
+                1. Self
+                2. Others`)
+                switch(toWho) {
+                    case 1:
+                        amount = +prompt(`Enter the amount to top up`);
+                        pin = +prompt(`Enter your pin`)
+                        newBalance = balance - amount;
+                        switch(pin){
+                            case 1234:
+                                alert(`Recharge Sucessful!!! Your new account balance is  ${currency}${newBalance}`);
+                                another()
+                            break;
+                            default:
+                                alert(`Enter the correct pin`);
+                                airtime();
+                        }
+                    break;
+                    case 2:
+                        let otherNumber = prompt(`Enter the beneficiary number`);
+                        amount = +prompt(`Enter the amount to top up`);
+                        pin = +prompt(`Enter your pin`)
+                        newBalance = balance - amount;
+                        switch(pin){
+                            case 1234:
+                                alert(`Recharge Sucessful!!! Your new account balance is ${currency}${newBalance}`);
+                                another()
+                            break;
+                            default:
+                                alert(`Enter the correct pin`);
+                                airtime();
+                        }
+                    break;
+                    default:
+                        alert(`Enter the available option`);
+                        airtime();
+                }
             }
+            airtime();
+        } else {
+            alert(`Kindly enter the available option`)
+            USSDTransaction();
         }
-}else {
-    alert(`Please enter the correct ussd code`)
+    }
+    USSDTransaction();
+    
+    
+    function another() {
+         let anotherOption = +prompt(`Do you want to perform another transaction?
+        1. Yes
+        2. No`)
+        if(anotherOption == 1) {
+            USSDTransaction();
+        }else{
+            alert(`Thank you for banking with us. Click Ok to exit`)
+        }
+    }
 }
-
-// let another = +prompt(`Do you want to perform another transaction?
-// 1. Yes
-// 2. No`)
-// if(another == 1) {
-//     do {
-//         option = option;
-//     } while(option)
-// }else{
-//     alert(`Click Ok to exit`)
-// }
